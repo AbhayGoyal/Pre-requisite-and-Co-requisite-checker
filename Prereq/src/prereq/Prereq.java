8bf8b40131f778d79d2d64fcef8d0c4c4a4b211b
Abhay Goyal <goyalabhay97@gmail.com>
2018-02-09 00:20:16 +0530

    First commit

---
+++ Prereq/src/prereq/Prereq.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prereq;
+
+/**
+ *
+ * @author Abhay Goyal
+ */
+public class Prereq {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
